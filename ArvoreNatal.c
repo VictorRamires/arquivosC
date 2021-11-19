21a5f2328bdc31ba810a90fba784703bf06edccf
VictorRamires <67079964+VictorRamires@users.noreply.github.com>
2021-11-19 00:53:45 -0400

    Add files via upload

---
+++ ArvoreNatal.c
@@ -0,0 +1,25 @@
+//exercicio 3040 uri
+#include<stdio.h>
+
+int main(){
+	
+	int altura, espessura, galhos, casos;
+
+	scanf("%d", &casos);
+
+	if(casos > 0 && casos < 10000){
+
+		for(int i = 0; i < casos; i++){
+			scanf("%d", &altura);
+			scanf("%d", &espessura);
+			scanf("%d", &galhos);
+
+			if(altura >= 200 && altura <= 300 && espessura >= 50 && galhos >= 150){
+				printf("Sim\n");
+			}
+			else 
+				printf("Nao\n");
+
+		}
+	}
+}
