24335b5d7c7798ae0a5ac47b7123854a3cf17f6b
VictorRamires <67079964+VictorRamires@users.noreply.github.com>
2022-08-11 21:07:54 -0400

    Update fibonacci_dinamico.cpp

--- fibonacci_dinamico.cpp
+++ fibonacci_dinamico.cpp
@@ -10,13 +10,6 @@ int fib(int n){
 //fibonacci normalmente
 	if(n <= 2)	return 1; 
 
-//Em termos semânticos, ao passar a variável para uma função onde o parâmetro é uma referência,
-//o endereço da variável é atribuído ao endereço do parâmetro. 
-//Desta forma, o parâmetro é a mesma variável passada, 
-//no trecho de código onde a função foi invocada, assumindo um nome diferente dentro da função. 
-//Podemos dizer que a variável assume um apelídio dentro da função,
-//sendo a mesma com nome diferente apenas.
-
 	int &valor = memo[n];
 	cout << "memo[n] = " << memo[n] << endl;
 //verifica se já há algum valor armazenado no vetor
